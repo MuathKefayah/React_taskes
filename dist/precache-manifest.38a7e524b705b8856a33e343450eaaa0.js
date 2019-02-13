d9eccbd9e7b73d332f2e6b27fad9e4b5dbaf8e9a
Muath Kefayah <muathkefayah@gmail.com>
2019-02-13 12:29:07 +0200

    First ReactJS Task

---
+++ dist/precache-manifest.38a7e524b705b8856a33e343450eaaa0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "588cd5ac8fb8bf606544",
+    "url": "/static/js/main.588cd5ac.chunk.js"
+  },
+  {
+    "revision": "2345088808eca074ace8",
+    "url": "/static/js/1.23450888.chunk.js"
+  },
+  {
+    "revision": "588cd5ac8fb8bf606544",
+    "url": "/static/css/main.ac537d50.chunk.css"
+  },
+  {
+    "revision": "94b062455572b3fb3f65003c12d2a1b1",
+    "url": "/index.html"
+  }
+];
